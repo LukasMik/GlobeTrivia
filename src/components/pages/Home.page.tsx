51d1dce552ce18856563b8f097788a5e7419a458
Lukáš Mikuláštík <mikulastik@slajit.cz>
2023-04-02 01:18:47 +0200

    edit HP

--- src/components/pages/Home.page.tsx
+++ src/components/pages/Home.page.tsx
@@ -3,13 +3,11 @@ import bgVideo from '../../assets/images/globe-video.mp4'
 const HomePage = () => {
     return (
         <>
-            <div className='bg-video'>
-                <video autoPlay muted loop>
-                    <source src={bgVideo} type="video/mp4"/>
-                </video>
-            </div>
+            <video autoPlay muted loop>
+                <source src={bgVideo} type="video/mp4"/>
+            </video>
             <div className='h-screen flex items-center justify-center z-10'>
-                <h1 className='transform -translate-y-16'>Home page</h1>
+                <h1 className='transform -translate-y-16'>Welcome to GlobeTrivia!</h1>
             </div>
         </>
     );
