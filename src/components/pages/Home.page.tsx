fdc33d929df214d294177c22c677ac0e163b2952
Lukáš Mikuláštík <mikulastik@slajit.cz>
2023-04-02 01:44:35 +0200

    fix HP menu

--- src/components/pages/Home.page.tsx
+++ src/components/pages/Home.page.tsx
@@ -3,7 +3,7 @@ import bgVideo from '../../assets/images/globe-video.mp4'
 const HomePage = () => {
     return (
         <>
-            <video autoPlay muted loop>
+            <video autoPlay muted loop className='absolute h-full w-full top-0 right-0 object-cover'>
                 <source src={bgVideo} type="video/mp4"/>
             </video>
             <div className='h-screen flex items-center justify-center z-10'>
