1b74bad1843a1891dd2294f4eb9072a5b5a4d3bd
Lukáš Mikuláštík <mikulastik@slajit.cz>
2023-04-02 21:01:26 +0200

    add search bar

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,42 @@
+import {ChangeEvent, FormEvent, useEffect, useRef, useState} from "react";
+
+interface Props {
+    placeholder?: string
+    btnLabel?: string
+    onQueryChange: (query: string) => void
+}
+
+const SearchBar = ({placeholder, btnLabel, onQueryChange}: Props) => {
+
+    const [query, setQuery] = useState('')
+
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+        onQueryChange(query)
+    }, [query]);
+    const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        setQuery('')
+    }
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setQuery(e.target.value)
+    }
+
+    return (
+        <div className="relative w-1/3 mx-auto mb-8">
+            <form className="flex items-center justify-center" onSubmit={onSubmit}>
+                <div className="relative w-full">
+                    <input type="text" placeholder={placeholder ?? 'Search'} ref={inputRef} value={query} onChange={onChange}
+                           className="w-full px-6 py-3 text-sm text-gray-700 placeholder-gray-400 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-gray-400"/>
+                    <button type="submit"
+                            className="absolute inset-y-0 right-0 px-6 text-sm font-medium text-white bg-gray-400 rounded-md shadow-md hover:bg-gray-500 focus:outline-none focus:ring-0 focus:border-gray-400 m-1">
+                        {btnLabel ?? 'Search'}
+                    </button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default SearchBar
--- src/components/pages/CountriesInfo.page.tsx
+++ src/components/pages/CountriesInfo.page.tsx
@@ -1,9 +1,12 @@
 import Header from "../Header";
 import {useQuery} from "react-query";
+import {useState} from "react";
 import axios from "axios";
 import CountryItem from "../CountryItem";
+import SearchBar from "../SearchBar";
 
 const CountriesInfoPage = () => {
+    const [search, setSearch] = useState('')
     const fetchCountries = () => axios.get('https://restcountries.com/v3.1/all')
 
     const {isLoading, isError, error, data} = useQuery('all-countries', fetchCountries)
@@ -14,15 +17,21 @@ const CountriesInfoPage = () => {
     if (isError)
         return <div className="mt-24 text-center text-2xl">{(error as Error).message}</div>
 
+    const onQueryChange = (query: string) => setSearch(query)
+
+    const filteredCountries = data?.data.filter((i: any) => i.name.common.toLowerCase().includes(search.toLowerCase()))
+
     return (
-        <>
+        <div className='p-8'>
             <Header heading='Countries information'
                     underHeading='Select a country and find out some interesting facts'/>
+            <SearchBar placeholder='Find your country!' onQueryChange={onQueryChange}/>
             <div className="flex flex-wrap mx-12 justify-center">
-                {data?.data.map((c: any) => <CountryItem key={c.name.common} item={c}/>)}
+                {filteredCountries.length === 0 ?
+                        <p className="text-center mt-12 text-3xl">No countries found..</p> :
+                        filteredCountries.map((i: any) => <CountryItem key={i.name.common} item={i}/>)}
             </div>
-        </>
+        </div>
     );
 }
-
 export default CountriesInfoPage;
